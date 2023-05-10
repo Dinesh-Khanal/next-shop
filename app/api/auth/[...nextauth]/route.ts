6a8910a0a09d5b2b37309e066d29e96af299e1f6
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-10 23:36:27 +0545

    initial commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
