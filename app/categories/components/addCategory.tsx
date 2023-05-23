95064828229e88fc0d530e028c12f67cbf412c25
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-05-23 19:28:57 +0545

    Add property to category completed

--- app/categories/components/addCategory.tsx
+++ app/categories/components/addCategory.tsx
@@ -7,6 +7,9 @@ function AddCategory() {
   const [catName, setCatName] = useState("");
   const [parentName, setParentName] = useState("");
   const [categories, setCategories] = useState<ICategory[]>();
+  const [properties, setProperties] = useState<
+    { name: string; value: string }[]
+  >([]);
   const router = useRouter();
   useEffect(() => {
     fetchCategories();
@@ -20,7 +23,7 @@ function AddCategory() {
     e.preventDefault();
     if (catName === "") return;
     axios
-      .post("/api/categories", { name: catName, parentName })
+      .post("/api/categories", { name: catName, parentName, properties })
       .then((result) => {
         if (result.status === 200) {
           router.refresh();
@@ -28,33 +31,82 @@ function AddCategory() {
       });
     setCatName("");
   }
+  function addProperties() {
+    setProperties((prev) => [...prev, { name: "", value: "" }]);
+  }
+  function propertyNameChange(index: number, newName: string) {
+    setProperties((prev) => {
+      const properties = [...prev];
+      properties[index].name = newName;
+      return properties;
+    });
+  }
+  function propertyValueChange(index: number, newValue: string) {
+    setProperties((prev) => {
+      const properties = [...prev];
+      properties[index].value = newValue;
+      return properties;
+    });
+  }
+  function deleteProperty(index: number) {
+    const newProperties = properties.filter((p, i) => i !== index);
+    setProperties(newProperties);
+  }
   return (
     <section>
       <h2 className="text-blue-900 font-semibold text-lg">New Category Name</h2>
-      <form onSubmit={handleSubmit} className="flex gap-2 mb-2">
-        <input
-          type="text"
-          placeholder="Category name"
-          className="py-1 px-2 border-2 rounded"
-          value={catName}
-          onChange={(e) => setCatName(e.target.value)}
-        />
-        <select
-          onChange={(ev) => setParentName(ev.target.value)}
-          value={parentName}
-          className="border-2"
-        >
-          <option value="">No parent category</option>
-          {categories &&
-            categories.map((category) => (
-              <option key={category._id} value={category.parentName}>
-                {category.name}
-              </option>
-            ))}
-        </select>
+      <form onSubmit={handleSubmit}>
+        <div className="flex gap-2 mb-2">
+          <input
+            type="text"
+            placeholder="Category name"
+            className="py-1 px-2 border-2 rounded"
+            value={catName}
+            onChange={(e) => setCatName(e.target.value)}
+          />
+          <select
+            onChange={(ev) => setParentName(ev.target.value)}
+            value={parentName}
+            className="border-2"
+          >
+            <option value="">No parent category</option>
+            {categories &&
+              categories.map((category) => (
+                <option key={category._id} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <h2 className="text-blue-900 text-lg font-semibold">Properties</h2>
+        <button type="button" onClick={addProperties} className="btn-default">
+          Add new property
+        </button>
+        {properties.map((property, index) => (
+          <div key={index} className="block my-2">
+            <input
+              type="text"
+              placeholder="Property name"
+              value={property.name}
+              onChange={(e) => propertyNameChange(index, e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Property value"
+              value={property.value}
+              onChange={(e) => propertyValueChange(index, e.target.value)}
+            />
+            <button
+              className="btn-red ml-1"
+              onClick={() => deleteProperty(index)}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
         <button
           type="submit"
-          className="bg-blue-900 text-white text-sm py-1 px-2 rounded"
+          className="bg-blue-900 text-white text-sm py-1 px-2 rounded block mt-2"
         >
           save
         </button>
--- types.d.ts
+++ types.d.ts
@@ -6,11 +6,15 @@ interface IProduct {
   images?: string[];
   category: string;
 }
-
+interface IProperty {
+  name: string;
+  value: string;
+}
 interface ICategory {
   _id?: string;
   name: string;
   parentName?: string;
+  properties?: IProperty[];
 }
 
 interface IParams {
